df999047b024b7524821a6e0af17313b78a2f1c4
CUNGVANTHANG <cungvanthang2k3@gmail.com>
2023-09-06 16:54:28 +0700

    Practive ReactJS

---
+++ Practive ReactJS/Dựng base dự án Tiktok/src/components/AccountItem/index.js
@@ -0,0 +1,23 @@
+import classNames from "classnames/bind";
+import styles from './AccountItem.module.scss'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+
+const cx = classNames.bind(styles)
+
+function AccountItem() {
+    return ( 
+        <div className={cx('wrapper')}>
+            <img className={cx('avatar')} src="" alt="Hoa" />
+            <div className={cx('info')}>
+                <p className={cx('name')}>
+                    <span>Nguyễn Văn A</span>
+                    <FontAwesomeIcon icon={faCheckCircle} />
+                </p>
+                <span className={cx('username')}>nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
--- Practive ReactJS/Dựng base dự án Tiktok/src/components/Layout/components/Header/Header/index.js
+++ Practive ReactJS/Dựng base dự án Tiktok/src/components/Layout/components/Header/Header/index.js
@@ -3,6 +3,7 @@ import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import Tippy from '@tippyjs/react/headless';
+import { Wrapper as PopperWrapper } from '~/components/Layout/Popper';
 import styles from './Header.module.scss';
 import images from '~/assets/images';
 
@@ -24,10 +25,14 @@ function Header() {
                 <Tippy 
                     interactive
                     visible={searchResult.length > 0}
-                    render={(attrs) => 
-                        (<div className={cx('search-result')} tabIndex="-1" {...attrs}>
-                            Kết quả
-                        </div>
+                    render={(attrs) => (
+                        <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                            <PopperWrapper>
+                                <h4 className={cx('search-title')}>
+                                    Accounts
+                                </h4>
+                            </PopperWrapper>
+                        </div>                        
                     )}
                 >
                     <div className={cx('search')}>
